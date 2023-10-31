e74e01a53db306200016ef9e937ba01b31c8f3f3
Gathin <gathint23@gmail.com>
2023-10-31 11:45:33 +0530

    Fix: Copy button block

--- src/components/Command/Command.js
+++ src/components/Command/Command.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const Command = ({ title,text, exampleOutput }) => {
+const Command = ({ title, text, exampleOutput }) => {
   const [copied, setCopied] = useState(false);
   const [showExampleOutput, setShowExampleOutput] = useState(false);
 
@@ -23,17 +23,21 @@ const Command = ({ title,text, exampleOutput }) => {
       <div className="mt-5 w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-3xl mx-auto">
         <p className="text-2xl font-satoshi">{title}</p>
       </div>
-      <div className="mt-4 w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-3xl mx-auto bg-black rounded-xl p-4 relative">
-        <div className="pl-2 text-white font-medium">{text}</div>
-        <button
-          className={`bg-white rounded-xl py-2 px-4 absolute top-2 right-2 ${
-            copied ? " cursor-not-allowed" : ""
-          }`}
-          onClick={handleCopyClick}
-          disabled={copied}
-        >
-          {copied ? "Copied!" : "Copy"}
-        </button>
+      <div className="mt-4 w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-3xl mx-auto bg-black rounded-xl p-2 relative">
+        <div className="text-white font-medium">
+          <div className="flex justify-between items-center">
+            <span>{text}</span>
+            <button
+              className={`bg-white text-black rounded-xl py-2 px-4 ${
+                copied ? "cursor-not-allowed" : ""
+              }`}
+              onClick={handleCopyClick}
+              disabled={copied}
+            >
+              {copied ? "Copied!" : "Copy"}
+            </button>
+          </div>
+        </div>
       </div>
       {showExampleOutput && (
         <div className="mt-2 w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-3xl mx-auto bg-black rounded-xl p-4 relative">
@@ -43,7 +47,7 @@ const Command = ({ title,text, exampleOutput }) => {
       <div className="mt-1 w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-3xl mx-auto">
         <button
           onClick={toggleExampleOutput}
-          className="text-gray-700 text-sm underline cursor-pointer "
+          className="text-gray-700 text-sm underline cursor-pointer"
         >
           {showExampleOutput ? "Hide Example Output" : "Show Example Output"}
         </button>
