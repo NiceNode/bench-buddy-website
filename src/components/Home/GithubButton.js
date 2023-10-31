e56d0305f1875308e57dcfc11043666d61c3dd66
Gathin <gathint23@gmail.com>
2023-10-31 12:04:32 +0530

    Upd: Github dynamic colour

--- src/components/Home/GithubButton.js
+++ src/components/Home/GithubButton.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import github from "../../images/github.svg";
+import githubWhite from "../../images/github_white.svg";
 
 const GithubButton = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
   const openGitHubPage = () => {
     window.open("https://github.com/NiceNode/speedometer", "_blank");
   };
@@ -9,10 +12,16 @@ const GithubButton = () => {
   return (
     <div>
       <button
-        className="bg-transparent border border-2 border-black font-bold rounded-3xl flex items-center p-2 mt-10 mb-10"
+        className="bg-transparent border border-2 border-black font-bold rounded-3xl flex items-center p-2 mt-10 mb-10 transition-all duration-300 hover:bg-black hover:text-white"
         onClick={openGitHubPage}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
       >
-        <img src={github} alt="Github" className="mr-2" />
+        <img
+          src={isHovered ? githubWhite : github}
+          alt="Github"
+          className="mr-2"
+        />
         View on Github
       </button>
     </div>
