63aa34a089e255633eef270ac92e271e19da29b0
Gathin <gathint23@gmail.com>
2023-10-31 13:03:53 +0530

    Add: Podman Command Template

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from './components/Home/Logo';
 import Heading from './components/Home/Heading';
 import GithubButton from './components/Home/GithubButton';
@@ -8,21 +8,28 @@ import About from './components/About/About';
 import Attributions from './components/About/Attributions';
 import CopyRight from './components/Footer/Copyright';
 import PreRequisite from './components/Home/PreRequisite';
-
+import Podman from './components/Command/Podman';
 
 function App() {
+  const [selectedComponent, setSelectedComponent] = useState("Docker");
+
+  const handleComponentChange = (component) => {
+    setSelectedComponent(component);
+  };
+
   return (
     <div className='bg-base flex flex-col justify-center items-center'>
-      <Logo/>
-      <Heading/>
-      <Description/>
-      <GithubButton/>
-      <PreRequisite/>
-      <Docker/>
+      <Logo />
+      <Heading />
+      <Description />
+      <GithubButton />
+      <PreRequisite onComponentChange={handleComponentChange} />
+      {selectedComponent === 'Docker' && <Docker />}
+      {selectedComponent === 'Podman' && <Podman />}
 
-      <About/>
-      <Attributions/>
-      <CopyRight/>
+      <About />
+      <Attributions />
+      <CopyRight />
     </div>
   );
 }
--- src/components/Home/PreRequisite.js
+++ src/components/Home/PreRequisite.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import SubHeading from "../SubHeading";
+import PropTypes from "prop-types";
 
-const PreRequisite = () => {
+const PreRequisite = ({ onComponentChange }) => {
+  const [selectedButton, setSelectedButton] = useState("Docker");
+
+  const handleButtonClick = (button) => {
+    setSelectedButton(button);
+    onComponentChange(button);
+  };
+
   return (
     <>
       <div className="p-10"></div>
       <SubHeading name="Pre-Requisite" />
       <div className="mt-5 w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-3xl mx-auto">
-        <p className="text-2xl font-satoshi">
+      <p className="text-2xl font-satoshi">
           A container engine like <span className="underline">Podman</span> or{" "}
           <span className="underline">Docker</span> is required to be installed
           before you can run the BenchBuddy commands.
@@ -17,13 +25,27 @@ const PreRequisite = () => {
         <p className="text-2xl font-satoshi">
           Show documentation below for{" "}
           <span className="inline-flex items-center">
-            <button className="bg-black text-2xl border border-2 border-black text-base-color font-bold rounded-2xl p-1 pl-3 pr-3 mx-2">
+            <button
+              className={`${
+                selectedButton === "Docker"
+                  ? "bg-black text-2xl border border-2 border-black text-base-color font-bold rounded-2xl p-1 pl-3 pr-3 mx-2"
+                  : "bg-transparent border border-2 border-gray-400 text-gray-400 font-bold rounded-2xl p-1 pl-3 pr-3 mx-2"
+              }`}
+              onClick={() => handleButtonClick("Docker")}
+            >
               Docker
             </button>
           </span>
           {" "}or{" "}
           <span className="inline-flex items-center">
-            <button className="bg-transparent border border-2 border-gray-400 text-gray-400 font-bold rounded-2xl p-1 pl-3 pr-3 mx-2">
+            <button
+              className={`${
+                selectedButton === "Podman"
+                  ? "bg-black text-2xl border border-2 border-black text-base-color font-bold rounded-2xl p-1 pl-3 pr-3 mx-2"
+                  : "bg-transparent border border-2 border-gray-400 text-gray-400 font-bold rounded-2xl p-1 pl-3 pr-3 mx-2"
+              }`}
+              onClick={() => handleButtonClick("Podman")}
+            >
               Podman
             </button>
           </span>
@@ -32,6 +54,10 @@ const PreRequisite = () => {
       <div className="p-10"></div>
     </>
   );
+};
+
+PreRequisite.propTypes = {
+  onComponentChange: PropTypes.func.isRequired,
 };
 
 export default PreRequisite;
