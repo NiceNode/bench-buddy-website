0b9b89e876f796d33aca30edfeeb4a5eba3fb42c
Gathin <gathint23@gmail.com>
2023-11-06 17:46:17 +0530

    fix: Logo responsiveness

--- src/components/Home/Logo.js
+++ src/components/Home/Logo.js
@@ -6,7 +6,7 @@ const Logo = () => {
     <div className="mt-[120px] mr-10">
       <img
         src={logo}
-        className="max-w-xs mx-auto w-full md:max-w-md lg:max-w-[214px] h-[197px]"
+        className="max-w-[214px] h-[197px] mx-auto md:max-w-md lg:max-w-[214px] lg:h-[197px]"
         alt="logo"
       />
     </div>
