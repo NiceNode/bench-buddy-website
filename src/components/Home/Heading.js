74bb135dc01faf4cac5e3b34177a7c8ca539f220
Gathin <gathint23@gmail.com>
2023-11-21 19:07:42 +0530

    Upd: Heading in text format

--- src/components/Home/Heading.js
+++ src/components/Home/Heading.js
@@ -3,12 +3,8 @@ import title from "../../images/title.png";
 
 const Heading = () => {
   return (
-    <div>
-      <img
-        src={title}
-        alt="Title"
-        className="mt-[24px] w-full max-w-[3333px] h-[60px] md:max-w-md lg:max-w-[840px] lg:h-[110px] mx-auto"
-      />
+    <div className="mt-[24px] mb-[16px]">
+      <h1 className="title-text">BenchBuddy</h1>
     </div>
   );
 };
